1b531eb40dc033509a245ffb5fe883b1e20c5bd7
rick <technoweenie@gmail.com>
2009-01-02 02:55:13 -0500

    add 'reason' to the question

--- islostonyet.rb
+++ islostonyet.rb
@@ -1,15 +1,23 @@
 require 'rubygems'
+gem 'sinatra', '~> 0.3'
 require 'sinatra'
+require 'json'
 
 get '/' do
   erb :index
 end
 
 get '/json' do
-  json = "no"
+  json = is_lost_on_yet?.to_json
   if params[:callback]
     "#{params[:callback]}(#{json})"
   else
     json
+  end
+end
+
+helpers do
+  def is_lost_on_yet?
+    @is_lost_on_yet ||= {:answer => "no", :reason => "returns on Jan 21st, 9PM ET"}
   end
 end
